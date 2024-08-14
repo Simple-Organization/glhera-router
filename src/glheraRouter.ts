e14a5c2a448ceb7d2dd5ec844e8d7a3a279b483c
AndreiLucas123 <andrei.lucas.goncalves@gmail.com>
2024-08-13 22:30:48 -0300

    Added router methods

--- src/glheraRouter.ts
+++ src/glheraRouter.ts
@@ -10,27 +10,63 @@ export interface GLHeraRouter {
   /** base route for router, like /glhera */
   base: string;
 
-  /** Not a signal, is used to avoid pushing the same URL */
-  lastURL: string;
+  /**
+   * Push a route based on url sent, will update the history api
+   *
+   * If the URL is the same will not update history api, but will update the queryObj signal anyway
+   */
+  push(url: string): void;
+  /**
+   * Push a route based on pathname and queryObj sent, will update the history api
+   *
+   * If the URL is the same will not update history api, but will update the queryObj signal anyway
+   */
+  push(pathname: string, queryObj: Record<string, any>): void;
+
+  /**
+   * Replace a route based on url sent, will update the history api
+   *
+   * If the URL is the same will not update history api, but will update the queryObj signal anyway
+   */
+  replace(url: string): void;
+  /**
+   * Replace a route based on pathname and queryObj sent, will update the history api
+   *
+   * If the URL is the same will not update history api, but will update the queryObj signal anyway
+   */
+  replace(pathname: string, queryObj: Record<string, any>): void;
+
+  /**
+   * Change the router state from a url string, does not update the history api
+   *
+   * The url needs to have http:// and origin to create a new URL object
+   */
+  popState(url: string): void;
 }
 
 //
 //
 
-export function glheraRouter(url: string, base = ''): GLHeraRouter {
+/**
+ * Create a router object that can be used to manage the state of the router
+ * @param url URL to create the router from
+ * @param base base route for router, like /glhera
+ * @param testing if true will not access the browser history api
+ */
+export function glheraRouter(
+  url: string,
+  base = '',
+  testing = false,
+): GLHeraRouter {
   //
   // Set the pathname and queryObj from the URL
-
-  let _url: URL;
-  let queryObj: Record<string, any>;
 
   if (url.startsWith('/')) {
     url = 'http://localhost' + url;
   }
 
-  _url = new URL(url);
-
-  queryObj = Object.fromEntries(_url.searchParams.entries());
+  const _url = new URL(url);
+  const queryObj = Object.fromEntries(_url.searchParams.entries());
 
   //
   //
@@ -42,10 +78,100 @@ export function glheraRouter(url: string, base = ''): GLHeraRouter {
   //
   //
 
+  const pathSignal = signalFactory<string>(_url.pathname);
+  const querySignal = signalFactory<Record<string, string>>(queryObj);
+  let lastURL = _url.pathname + _url.search;
+
+  //
+  //
+
+  function popState(url: string): void {
+    //
+    // Set the router's URL and query parameters
+
+    const _url = new URL(url);
+    const _query = {} as Record<string, string>;
+
+    for (const [key, value] of _url.searchParams.entries()) {
+      _query[key] = value;
+    }
+
+    if (_url.pathname.startsWith(base)) {
+      _url.pathname = _url.pathname.slice(base.length);
+    }
+
+    let newURL = _url.pathname;
+    const queryStr = _url.searchParams.toString();
+
+    if (queryStr) {
+      newURL += '?' + queryStr;
+    }
+
+    pathSignal.value = _url.pathname;
+    querySignal.value = _query;
+    lastURL = newURL;
+  }
+
+  //
+  //
+
+  /**
+   * Internal method used by pushRoute and replaceRoute
+   */
+  function _updateURLSignal(
+    pathname: string,
+    queryObj: Record<string, string> | undefined,
+    historyMethod: 'pushState' | 'replaceState',
+  ): void {
+    if (!queryObj) {
+      const _url = new URL(pathname);
+      queryObj = Object.fromEntries(_url.searchParams.entries());
+      pathname = _url.pathname;
+    }
+
+    pathSignal.value = pathname;
+    querySignal.value = queryObj;
+
+    let newURL = pathname;
+    const queryStr = new URLSearchParams(queryObj).toString();
+
+    if (queryStr) {
+      newURL += '?' + queryStr;
+    }
+
+    if (lastURL === newURL) {
+      return;
+    }
+
+    lastURL = newURL;
+
+    //
+    // Only access browser api like history if not in testing
+    if (!testing) {
+      history[historyMethod](null, '', base + pathname);
+    }
+  }
+
+  //
+  //
+
+  function replace(pathname: string, queryObj?: Record<string, any>): void {
+    _updateURLSignal(pathname, queryObj, 'replaceState');
+  }
+
+  function push(pathname: string, queryObj?: Record<string, any>): void {
+    _updateURLSignal(pathname, queryObj, 'pushState');
+  }
+
+  //
+  //
+
   return {
-    pathname: signalFactory(_url.pathname),
-    query: signalFactory(queryObj),
-    lastURL: _url.pathname + _url.search,
+    pathname: pathSignal,
+    query: querySignal,
+    push,
+    replace,
+    popState,
     base,
   };
 }
